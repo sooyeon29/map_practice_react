6a89aae5b79b247827c180fd39cc8dd93e9ceccd
sooyun <so__oyeonii@naver.com>
2022-10-13 17:09:07 +0900

    카카오지도api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Likes from "./pages/likes";
+import Map from "./redux/modules/kakaomap";
 
 export default function App() {
-  return <Likes />;
+  return (
+    <>
+      <Likes />
+    </>
+  );
 }
--- src/components/listing.jsx
+++ src/components/listing.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import { heartCounter } from "../redux/modules/changer";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 export default function Listing() {
   const globalcomment = useSelector((state) => state.myChanger.comments);
@@ -14,6 +15,15 @@ export default function Listing() {
   console.log(globalHeart);
   return (
     <>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height: "360px" }}
+      >
+        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Hello World!</div>
+        </MapMarker>
+      </Map>
+
       {globalcomment.map((commentList) => {
         return (
           <div key={commentList.id}>
